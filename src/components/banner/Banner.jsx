c4cb3c82b0068431a32c5e0b75e84be577154980
Otabek <abdullayevriza80@gmail.com>
2026-05-11 21:40:22 +0500

    salom

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -9,10 +9,10 @@ function Banner() {
         .then(data => setUser(data.recipes))
     }, [])
   return (
-    <div className="m-auto w-[2800px] mt-[40px]">
+    <div className="m-auto w-[2300px] mt-[40px]">
       <div className=" grid grid-cols-4 gap-[40px]">{user.map(item => (
-        <div className="flex items-center justify-between w-[670px] h-[300px] rounded-[16px] border-2 border-[#c9c9c9]" key={item.id}>
-            <img className="w-[300px] rounded-l-[16px]" src={item.image} alt="image" />
+        <div className="flex items-center justify-between w-[550px] h-[250px] rounded-[16px] border-2 border-[#c9c9c9]" key={item.id}>
+            <img className="w-[250px] rounded-l-[16px]" src={item.image} alt="image" />
 
             <div className="p-[30px]">
                 <h2></h2>
