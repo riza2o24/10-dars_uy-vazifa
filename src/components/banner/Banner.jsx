d030677c9364f5cb4e8ca5dbe80192a580067213
Otabek <abdullayevriza80@gmail.com>
2026-05-11 21:24:31 +0500

    wert

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,19 +4,19 @@ function Banner() {
     const [user, setUser] = useState([])
 
     useEffect(() => {
-        fetch("")
-        .then(rej => rej.json(""))
-        .then(data => setUser(data))
+        fetch("https://dummyjson.com/recipes")
+        .then(rej => rej.json())
+        .then(data => setUser(data.recipes))
     }, [])
   return (
-    <div className="m-auto w-[1980px] max-[1980px]:">
-      <div>{user.map(item => (
+    <div className="m-auto w-[1980px] max-[1980px]">
+      <div className=" grid grid-cols-4 gap-[40px]">{user.map(item => (
         <div className="flex items-center justify-between" key={item.id}>
             <div>
 
             </div>
 
-            <img src={item.image} alt="" />
+            <img src={item.image} alt="image" />
         </div>
       ))}</div>
     </div>
